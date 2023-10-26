38e1069291225e80be3d0b8d0ea9319c9a7ce7bd
Viddyyy <sanderknutsen125@gmail.com>
2023-10-27 00:25:47 +0200

    added search to categories page.

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -6,24 +6,75 @@ import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 /// Displays a grid of meal categories.
-class CategoriesScreen extends StatelessWidget {
-  /// Creates a [CategoriesScreen].
-  ///
-  /// The [availableMeals] parameter is required and specifies the list of all meals that can be filtered and displayed based on category selection.
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({
-    super.key,
     required this.availableMeals,
-  });
+  }) ;
 
   final List<Meal> availableMeals;
 
-  /// Handles the action of selecting a category.
-  ///
-  /// This function filters the available meals based on the selected category and navigates to the [MealsScreen] with the filtered meals and category title.
-  void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = availableMeals
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  TextEditingController _searchController = TextEditingController();
+  List<Meal> filteredMeals = [];
+  bool showSearchResults = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  void onSearch(String value) {
+    setState(() {
+
+      if (value.isNotEmpty) {
+        showSearchResults = true;
+        filteredMeals = widget.availableMeals.where((meal) {
+          return meal.title.toLowerCase().contains(value.toLowerCase());
+        }).toList();
+      } else {
+
+        showSearchResults = false;
+        filteredMeals.clear();
+      }
+    });
+  }
+
+  void clearSearch() {
+    setState(() {
+      _searchController.clear();
+      showSearchResults = false;
+      filteredMeals.clear();
+    });
+  }
+
+  void selectCategory(BuildContext context, Category category) {
+
+    filteredMeals = widget.availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
+
+    onSearch(filteredMeals.isNotEmpty ? filteredMeals[0].title : '');
 
     Navigator.of(context).push(
       MaterialPageRoute(
@@ -37,23 +88,124 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(24),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-      ),
-      children: [
-        for (final category in availableCategories)
-          CategoryGridItem(
-            category: category,
-            onSelectCategory: () {
-              _selectCategory(context, category);
-            },
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _searchController,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      labelText: 'Search Meals',
+                      prefixIcon: Icon(Icons.search),
+                    ),
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    onChanged: onSearch,
+                  )
+
+                ),
+                if (_searchController.text.isNotEmpty)
+                  Container(
+                    margin: const EdgeInsets.only(left: 8.0),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey[700],
+                    ),
+                    child: IconButton(
+                      icon: Icon(Icons.clear),
+                      onPressed: clearSearch,
+                    ),
+                  ),
+              ],
+            ),
           ),
-      ],
+          if (showSearchResults)
+            Expanded(
+                child: ListView.builder(
+                  itemCount: filteredMeals.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (ctx) => MealsScreen(
+                              title: filteredMeals[index].title,
+                              meals: [filteredMeals[index]],
+                            ),
+                          ),
+                        );
+                      },
+                      leading: Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              image: NetworkImage(filteredMeals[index].imageUrl),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                          child: Image.network(filteredMeals[index].imageUrl)),
+                      title: Text(
+                        filteredMeals[index].title,
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    );
+                  },
+                )
+
+            ),
+          if (!showSearchResults)
+            Expanded(
+              child: GridView(
+                padding: const EdgeInsets.all(24),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 3 / 2,
+                  crossAxisSpacing: 20,
+                  mainAxisSpacing: 20,
+                ),
+                children: [
+                  for (final category in availableCategories)
+                    CategoryGridItem(
+                      category: category,
+                      onSelectCategory: () {
+                        selectCategory(context, category);
+                      },
+                    )
+                ],
+              ),
+            ),
+        ],
+      ),
+      builder: (context, child) => SlideTransition(
+        position: Tween(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.easeInOut,
+          ),
+        ),
+        child: child,
+      ),
     );
   }
 }
--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -8,63 +8,104 @@ class FiltersScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final activeFilters = ref.watch(filtersProvider);
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Your Filters')),
+      appBar: AppBar(
+        title: const Text('Your Filters'),
+      ),
       body: Column(
         children: [
-          // A SwitchListTile to enable/disable gluten-free filter
-          _buildSwitchListTile(
-            context: context,
-            ref: ref,
-            filter: Filter.glutenFree,
-            title: 'Gluten-free',
-            subtitle: 'Only include gluten-free meals.',
+          SwitchListTile(
+            value: activeFilters[Filter.glutenFree]!,
+            onChanged: (isChecked) {
+              ref
+                  .read(filtersProvider.notifier)
+                  .setFilter(Filter.glutenFree, isChecked);
+            },
+            title: Text(
+              'Gluten-free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Only include gluten-free meals.',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
           ),
-          // A SwitchListTile to enable/disable lactose-free filter
-          _buildSwitchListTile(
-            context: context,
-            ref: ref,
-            filter: Filter.lactoseFree,
-            title: 'Lactose-free',
-            subtitle: 'Only include lactose-free meals.',
+          SwitchListTile(
+            value: activeFilters[Filter.lactoseFree]!,
+            onChanged: (isChecked) {
+              ref
+                  .read(filtersProvider.notifier)
+                  .setFilter(Filter.lactoseFree, isChecked);
+            },
+            title: Text(
+              'Lactose-free',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Only include lactose-free meals.',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
           ),
-          // A SwitchListTile to enable/disable vegetarian filter
-          _buildSwitchListTile(
-            context: context,
-            ref: ref,
-            filter: Filter.vegetarian,
-            title: 'Vegetarian',
-            subtitle: 'Only include vegetarian meals.',
+          SwitchListTile(
+            value: activeFilters[Filter.vegetarian]!,
+            onChanged: (isChecked) {
+              ref
+                  .read(filtersProvider.notifier)
+                  .setFilter(Filter.vegetarian, isChecked);
+            },
+            title: Text(
+              'Vegetarian',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Only include vegetarian meals.',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
           ),
-          // A SwitchListTile to enable/disable vegan filter
-          _buildSwitchListTile(
-            context: context,
-            ref: ref,
-            filter: Filter.vegan,
-            title: 'Vegan',
-            subtitle: 'Only include vegan meals.',
+          SwitchListTile(
+            value: activeFilters[Filter.vegan]!,
+            onChanged: (isChecked) {
+              ref
+                  .read(filtersProvider.notifier)
+                  .setFilter(Filter.vegan, isChecked);
+            },
+            title: Text(
+              'Vegan',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              'Only include vegan meals.',
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
           ),
         ],
       ),
-    );
-  }
-
-  Widget _buildSwitchListTile({
-    required BuildContext context,
-    required WidgetRef ref,
-    required Filter filter,
-    required String title,
-    required String subtitle,
-  }) {
-    final activeFilter = ref.watch(filtersProvider.select((filters) => filters[filter]!));
-    return SwitchListTile(
-      value: activeFilter,
-      onChanged: (isChecked) => ref.read(filtersProvider.notifier).setFilter(filter, isChecked),
-      title: Text(title, style: Theme.of(context).textTheme.titleLarge!.copyWith(color: Theme.of(context).colorScheme.onBackground)),
-      subtitle: Text(subtitle, style: Theme.of(context).textTheme.labelMedium!.copyWith(color: Theme.of(context).colorScheme.onBackground)),
-      activeColor: Theme.of(context).colorScheme.tertiary,
-      contentPadding: const EdgeInsets.only(left: 34, right: 22),
     );
   }
 }
